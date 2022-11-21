31a496c4842551e24e92374609a89ac3b68d916a
Tairone Livinalli <taironelivinalli@hotmail.com>
2022-11-21 16:19:11 -0500

    refactor: remove error property from response body

--- src/presentation/controllers/signUpController.ts
+++ src/presentation/controllers/signUpController.ts
@@ -3,17 +3,13 @@ export class SignUpController {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: {
-          error: new Error('Missing param: name')
-        }
+        body: new Error('Missing param: name')
       }
     }
     if (!httpRequest.body.email) {
       return {
         statusCode: 400,
-        body: {
-          error: new Error('Missing param: email')
-        }
+        body: new Error('Missing param: email')
       }
     }
   }
