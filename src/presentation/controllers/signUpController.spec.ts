6b56fde7e63bdb7719266eb93101853e78a1cfd8
Tairone Livinalli <taironelivinalli@hotmail.com>
2022-11-21 16:17:54 -0500

    test: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signUpController.spec.ts
+++ src/presentation/controllers/signUpController.spec.ts
@@ -3,16 +3,35 @@ import { SignUpController } from './signUpController'
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
+
     const httpRequest = {
       body: {
-        name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
       }
     }
+
     const httpResponse = sut.handle(httpRequest)
+
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual({ error: new Error('Missing param: name') })
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual({ error: new Error('Missing param: email') })
   })
 })
