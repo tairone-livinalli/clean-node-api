d0ea850b5e4e420ca3cad564a8c9ff09c92baa28
Tairone Livinalli <taironelivinalli@hotmail.com>
2022-11-21 19:30:41 -0500

    test: ensure SignUpController calls EmailValidator with correct email

--- src/presentation/controllers/signUpController.spec.ts
+++ src/presentation/controllers/signUpController.spec.ts
@@ -97,7 +97,7 @@ describe('SignUp Controller', () => {
 
     const httpRequest = {
       body: {
-        name: ' ',
+        name: 'any_name',
         email: 'invalid_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
@@ -108,5 +108,24 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+  })
+
+  test('Should call EmailValidator with correct email ', () => {
+    const { sut, emailValidatorStub } = makeSut()
+
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    sut.handle(httpRequest)
+
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
