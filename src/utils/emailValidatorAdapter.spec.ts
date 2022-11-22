50646f2b42b3060b21e402fa546eedeb222d29fe
Tairone Livinalli <taironelivinalli@hotmail.com>
2022-11-22 10:39:51 -0500

    test: ennsure EmailValidatorAdapter calls validator with correct email

--- src/utils/emailValidatorAdapter.spec.ts
+++ src/utils/emailValidatorAdapter.spec.ts
@@ -20,8 +20,17 @@ describe('EmailValidator Adapter', () => {
 
   test('Should returns true if validator returns true', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@mail.com')
+    const isValid = sut.isValid('valid_email@mail.com')
 
     expect(isValid).toBe(true)
+  })
+
+  test('Should returns false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+
+    sut.isValid('any_email@mail.com')
+
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
