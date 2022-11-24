d4ab33e1bc29448c46f774772642605e489c0db7
Tairone Livinalli <taironelivinalli@hotmail.com>
2022-11-23 19:08:39 -0500

    refactor: clean database between tests

--- src/infra/db/mongodb/account-repository/accountRepository.spec.ts
+++ src/infra/db/mongodb/account-repository/accountRepository.spec.ts
@@ -10,6 +10,12 @@ describe('Account Mongo Repository', () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
 
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+
+    await accountCollection.deleteMany({})
+  })
+
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
