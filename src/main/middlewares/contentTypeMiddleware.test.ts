79f7e39dc89ce749bc1b4eee706090c8ae2852c0
Tairone Livinalli <taironelivinalli@hotmail.com>
2022-11-29 10:59:43 -0500

    test: ensure requests return xml when override default content type

--- src/main/middlewares/contentTypeMiddleware.test.ts
+++ src/main/middlewares/contentTypeMiddleware.test.ts
@@ -12,4 +12,15 @@ describe('Content Type Middleware', () => {
       .get('/test-content-type')
       .expect('content-type', /json/)
   })
+
+  test('Should return content type as xml when override', async () => {
+    app.get('/test-content-type-xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+
+    await request(app)
+      .get('/test-content-type-xml')
+      .expect('content-type', /xml/)
+  })
 })
