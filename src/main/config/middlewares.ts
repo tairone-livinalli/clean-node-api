e9667519866be083aa3524f69e1c5d3fa189ba78
Tairone Livinalli <taironelivinalli@hotmail.com>
2022-11-29 10:57:58 -0500

    feat: ensure requests return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewares/bodyParserMiddleware'
+import { contentType } from '../middlewares/contentTypeMiddleware'
 import { cors } from '../middlewares/corsMiddleware'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
