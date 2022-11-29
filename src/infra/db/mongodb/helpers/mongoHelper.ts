7e0a8495550d43f5d6dd21c751a273a78d254ebd
Tairone Livinalli <taironelivinalli@hotmail.com>
2022-11-29 11:47:17 -0500

    feat: connect to mongodb before app runs

--- src/infra/db/mongodb/helpers/mongoHelper.ts
+++ src/infra/db/mongodb/helpers/mongoHelper.ts
@@ -4,7 +4,7 @@ export const MongoHelper = {
   mongoClient: null as MongoClient,
 
   async connect (url: string): Promise<void> {
-    this.mongoClient = await MongoClient.connect(process.env.MONGO_URL)
+    this.mongoClient = await MongoClient.connect(url)
   },
 
   async disconnect (): Promise<void> {
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongoHelper'
+import env from './config/env'
 
-app.listen(3001, () => console.log('🚀 Server running at http://localhost:3001'))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`🚀 Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
